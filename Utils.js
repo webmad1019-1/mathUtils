89f454b24701fa2c17867e4962404f11c9ee2a71
dani <danivicario@dvicario.es>
2019-10-30 12:32:45 +0100

    added type checking

--- Utils.js
+++ Utils.js
@@ -1,8 +1,11 @@
 let UtilsDani = {
-    randomFloat(min, max) {
-        return Math.random() * (max - min) + min;
-    }, 
-    randomInt(min, max){
-        return Math.floor(Math.random() * (max - min + 1) + min);
+  randomFloat(min, max) {
+    if (typeof min !== "number" || typeof max !== "number") {
+      throw new TypeError("Wrong value as argument");
     }
-}
+    return Math.random() * (max - min) + min;
+  },
+  randomInt(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) + min);
+  }
+};
